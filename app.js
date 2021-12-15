8a3adcb1119c3ba6a566a34ed31a3ef5c36d8f55
matiassemelman <matiassemelman@gmail.com>
2021-12-15 18:02:20 -0300

    some changes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // 1. Given a string, make a function that reverses the char order
 function reverseStr(str) {
     let textReverse = "";
@@ -28,20 +27,39 @@ function destructuringReverse(str) {
     return arrayOfStr.reverse();
     }
 // ============================ TEST =============================
-let secondExample = document.getElementById("secondExample").innerHTML;
+let destructuringExample = document.getElementById("destructuringExample").innerHTML;
 let secondButton = document.getElementById("secondButton");
 
-secondButton.addEventListener('click', function destructuringReverse() {
-    document.getElementById('secondExample').innerHTML = reverseStr(secondExample);
+secondButton.addEventListener('click', function destructuring() {
+    document.getElementById('destructuringExample').innerHTML = destructuringReverse(destructuringExample).join("");
 })
 
 
 
 // 3. Given a string, reverse the order of chars recursively.
 
+function recursiveReverse(str) {
+    if (str === "") {
+        return "";
+    } else {
+        let subStr = str.substring(1)
+        return reverseStr(subStr) + str.charAt[0]
+    }
+}
+
+// ============================ TEST =============================
+let recursiveExample = document.getElementById("recursiveExample").innerHTML;
+let recursiveButton = document.getElementById('recursiveButton');
+
+recursiveButton.addEventListener('click', function recursive() {
+    document.getElementById('recursiveExample').innerHTML = recursiveReverse(recursiveExample)
+})
+
 
 
 // 5. Build a function isPalindrome which, given a string, tell us if is palindrome or not.
+
+
 
 
 
